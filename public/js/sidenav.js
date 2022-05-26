f80aa5305efe2f724511f504cb42501e6ae53673
Cristobal Marquez-Glynn <cristobalmqz@gmail.com>
2022-05-25 18:22:43 -0700

    Created first HTML handlebars pages: main, homepage, login and signup

---
+++ public/js/sidenav.js
@@ -0,0 +1,5 @@
+//Event listener for the side navigation when screen is less than 992px
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems);
+  });
